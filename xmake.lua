1621886e626d868cfc54102ec6c17bb8762ffc93
lily <ilovesuno@proton.me>
2026-04-14 19:47:53 +0000

    feat: xmake testing command

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,17 @@
 set_languages("c++23")
 
-target("test")
+target("adn")
     set_kind("binary")
-
     add_files("src/**.cpp")
-    add_headerfiles("src/**.h", "src/**.hpp")
+
+task("test")
+    set_menu {
+        usage = "xmake test"
+    }
+    on_run(function ()
+        local current_dir = os.curdir()
+        local input = path.join(current_dir, "examples", "test.adn")
+        
+        os.exec("xmake build -v adn")
+        os.execv("xmake", { "run", "adn", input })
+    end)
